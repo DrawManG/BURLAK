98910481d5952931cc01e2bb468e818f57d40291
Дмитрий Смирнов <we-are-legion2013@yandex.ru>
2019-09-27 16:59:42 +0300

    first comit

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace BURLAK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.X = new System.Windows.Forms.TextBox();
+            this.Y = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(653, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(306, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Загрузить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(653, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(306, 398);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ширина";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Высота";
+            // 
+            // X
+            // 
+            this.X.Location = new System.Drawing.Point(102, 93);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(100, 20);
+            this.X.TabIndex = 4;
+            this.X.Text = "600";
+            // 
+            // Y
+            // 
+            this.Y.Location = new System.Drawing.Point(102, 122);
+            this.Y.Name = "Y";
+            this.Y.Size = new System.Drawing.Size(100, 20);
+            this.Y.TabIndex = 5;
+            this.Y.Text = "825";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(12, 302);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(623, 147);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 461);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Y);
+            this.Controls.Add(this.X);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Burlak Olya";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox X;
+        private System.Windows.Forms.TextBox Y;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BURLAK
+{
+    public partial class Form1 : Form
+    {
+        public string filename;
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //открыть файл
+                if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                    return;
+                // получаем выбранный файл
+                filename = openFileDialog1.FileName;
+                //вставляем в окно
+                Bitmap image1 = new Bitmap(filename);
+                pictureBox1.Size = new System.Drawing.Size(150, 150);
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+                pictureBox1.Image = image1;
+            }
+            catch
+            {
+                MessageBox.Show("Взяли не картинку");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //запускаем стрим
+                Stream myStream;
+                //выбираем путь сохранения
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog
+                {
+                    Filter = "picture (*.jpg)|*.jpg|All files (*.jpg)|*.jpg",
+                    FilterIndex = 1,
+                    RestoreDirectory = true
+                };
+                //если путь доступен то сохраняем как нужно
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if ((myStream = saveFileDialog1.OpenFile()) != null)
+                    {
+                        //Bitmap bmp = new Bitmap(Image.FromFile(filename), 555, 555);
+                        //bmp.Dispose();
+
+                        Bitmap b1 = new Bitmap(openFileDialog1.FileName);
+                        Bitmap b2 = new Bitmap(b1, new Size(Convert.ToInt32(X.Text), Convert.ToInt32(Y.Text)));
+                        b2.Save(saveFileDialog1.FileName + ".jpg ");
+                        MessageBox.Show(saveFileDialog1.FileName + ".jpg " + " с размерами " + X.Text + " на " + Y.Text);
+                        //  bmp.Save(saveFileDialog1.FileName);
+
+                    }
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Не указали формат фотографии или указали в папку куда сохранить нельзя!");
+            }
+        }
+
+    } }
+
